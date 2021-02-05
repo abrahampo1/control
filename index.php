8883508f582d40edda830b72b06cd477c102c543
abrahampo1 <abraham@cpsoftware.es>
2021-02-05 12:16:26 +0100

    auditar

--- index.php
+++ index.php
@@ -308,7 +308,11 @@ if(isset($_POST["operario_inc"]) && isset($_POST["accidente"]))
 					  <a href="#" class="btn btn-white" data-bs-toggle="modal" data-bs-target="#hacer-cambio">
 					   Hacer un cambio
 					  </a>
-					</span><a href="#" class="btn btn-danger d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
+					</span><span class="d-none d-sm-inline">
+					<a href="#" class="btn btn-white" data-bs-toggle="modal" data-bs-target="#auditar">
+					 Auditar
+					</a>
+				  </span><a href="#" class="btn btn-danger d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="12" y1="5" x2="12" y2="19" /><line x1="5" y1="12" x2="19" y2="12" /></svg>
                     Reportar una irregularidad
                   </a>
@@ -711,6 +715,56 @@ $do = mysqli_query($link, $sql);
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="mb-3">
+                  <label class="form-label">Cambio en la planificación</label>
+                  <div class="input-group input-group-flat">
+                    <span class="input-group-text">
+                    </span>
+                    <input type="text" name="input_cambio" required class="form-control ps-0"  value="" placeholder="Escribe aqui..." autocomplete="off">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+          <div class="modal-footer">
+            <a href="#" class="btn btn-link link-secondary" data-bs-dismiss="modal">
+              Cancelar
+            </a>
+            <button type="submit" class="btn btn-primary ms-auto">
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="12" y1="5" x2="12" y2="19" /><line x1="5" y1="12" x2="19" y2="12" /></svg>
+              Reportar Cambio
+            </button>
+          </div>
+		  </form>
+        </div>
+      </div>
+	</div>
+	<div class="modal modal-blur fade" id="hacer-cambio" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+		<form action="" method="post">
+          <div class="modal-header">
+            <h5 class="modal-title">Auditar</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+		  <div class="mb-3">
+              <label class="form-label">Operario Afectado</label>
+              <select name="operario_inc" required id="" class="form-select">
+			  <?php
+			  $sql = "SELECT * FROM personal";
+			  if($do = mysqli_query($link, $sql)){
+				  echo('<option value="" selected>Seleccione</option>');
+			 while($row = mysqli_fetch_assoc($do))
+			 {
+				 echo('<option value='.$row["id"].'>'.$row["num_operario"].' - '.$row['nombre'].'</option>');
+			 } }
+			  ?>
+			  </select>
+            </div>
             <div class="row">
               <div class="col-lg-12">
                 <div class="mb-3">
